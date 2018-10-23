8253b61efc8f79848b97cc598eebfc90f13d6b49
Dona Paulson <donavpaulson@gmail.com>
2018-10-23 16:07:44 +0530

    Initial commit

---
+++ app/src/main/java/com/login_page/hp/retrofitexample/GetDataService.java
@@ -0,0 +1,12 @@
+package com.login_page.hp.retrofitexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/photos")
+    Call<List<RetroPhoto>> getAllPhotos();
+}
